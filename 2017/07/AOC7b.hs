ba087755e3f9d42aeea9419ec10a13dd8046f8b6
gfixler <gary@garyfixler.com>
2017-12-07 06:14:56 -0800

    Build map from parsed input

--- 2017/07/AOC7b.hs
+++ 2017/07/AOC7b.hs
@@ -1,17 +1,18 @@
 module AOC7b where
 
+import qualified Data.Map as M (empty, fromList, insert)
+
 type Name = String
 type Weight = Int
 
-data Tower = Tower Name Weight [Name] deriving Show
-
-parse :: [String] -> Tower
-parse (n:w:r) = Tower n (read $ init $ tail w) (f r)
+parse :: [String] -> (Name, (Weight, [Name]))
+parse (n:w:r) = (n, (read $ init $ tail w, f r))
     where f [] = []
           f (_:ts) = map init ts
 
 main :: IO ()
 main = do
     s <- readFile "input.txt"
-    mapM_ print $ map (parse . words) $ lines s
+    let m = M.fromList (map (parse . words) $ lines s)
+    print m
 
