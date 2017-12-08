6c27e479e498972d5b0611c9a12c7148f521781e
gfixler <gary@garyfixler.com>
2017-12-08 02:50:05 -0800

    Add puzzle 8, part 1 solution in Haskell

--- 2017/08/AOC08a.hs
+++ 2017/08/AOC08a.hs
@@ -4,40 +4,52 @@ import qualified Data.Map as M ((!), Map, elems, empty, insertWith, lookup)
 import Debug.Trace
 
 type Reg = String
-type Comp = M.Map String Int
+type CPU = M.Map String Int
 
-type Op = Int -> Int -> Int
-type Instr = Int -> Int
-type Check = Int -> Int -> Bool
-type Cond = Comp -> Bool
-
-type Cmd = (Reg, Instr, Cond)
+data Op = Inc | Dec deriving (Eq, Ord, Show)
+data CondOp = OpLT | OpGT | OpLE | OpGE | OpEQ | OpNE deriving (Eq, Ord, Show)
+type Cond = (Reg, CondOp, Int)
+type Instr = (Reg, Op, Int)
 
 readInt :: String -> Int
 readInt = read
 
 parseOp :: String -> Op
-parseOp "inc" = flip (+)
-parseOp "dec" = flip (-)
+parseOp "inc" = Inc
+parseOp "dec" = Dec
 
-parseCond :: String -> Check
-parseCond "<" = (<)
-parseCond ">" = (>)
-parseCond "<=" = (<=)
-parseCond ">=" = (>=)
-parseCond "==" = (==)
-parseCond "!=" = (/=)
+parseCond :: String -> CondOp
+parseCond "<" = OpLT
+parseCond ">" = OpGT
+parseCond "<=" = OpLE
+parseCond ">=" = OpGE
+parseCond "==" = OpEQ
+parseCond "!=" = OpNE
 
-parse :: [String] -> Cmd
-parse [r, op, n, _, r', c, n'] = (r, parseOp op (readInt n), chk)
-    where chk comp = (parseCond c) (maybe 0 id (M.lookup r' comp)) (readInt n')
+parseCondInstr :: [String] -> (Instr, Cond)
+parseCondInstr [r1, o, n, _, r2, c, k] = ( (r1, parseOp o,   readInt n)
+                                         , (r2, parseCond c, readInt k) )
 
-run :: Cmd -> Comp -> Comp
-run (r, i, c) comp = M.insertWith (\n o -> if c' then i n else o) r (i 0) comp
-    where c' = c comp
+getCond :: CondOp -> Int -> Int -> Bool
+getCond OpLT = (<)
+getCond OpGT = (>)
+getCond OpLE = (<=)
+getCond OpGE = (>=)
+getCond OpEQ = (==)
+getCond OpNE = (/=)
+
+check :: CPU -> Cond -> Bool
+check cpu (reg, cmp, n) = getCond cmp v n
+    where v = maybe 0 id $ M.lookup reg cpu
+
+update :: Instr -> CPU -> CPU
+update (reg, op, n) = M.insertWith (+) reg (n * if op == Inc then 1 else -1)
+
+run :: (Instr, Cond) -> CPU -> CPU
+run (i, c) cpu = if check cpu c then update i cpu else cpu
 
 main :: IO ()
 main = do
-    cmds <- fmap (map (parse . words) . lines) $ readFile "input.txt"
+    cmds <- fmap (map (parseCondInstr . words) . lines) $ readFile "input.txt"
     print $ maximum $ M.elems $ foldl (flip run) M.empty cmds
 
