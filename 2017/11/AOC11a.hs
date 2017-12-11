782f6fd6d89a15c22ea170b54036227a66a11b03
gfixler <gary@garyfixler.com>
2017-12-10 21:43:24 -0800

    Add puzzle 11, part 1 "solution"
    I commmitted this as failing at first, but only as I'd understood the
    output wrong. This doesn't calculate the final result; it just tells you
    where you end up on my representation of a hex grid.
    
    If you look at a hex such that the tops and bottoms of the cells are
    horizontal, and then take the zig-zagging verticals and flatten them,
    you get a grid that's simply offset up or down each row:
      ____        ____        ____       ______        ______        ______
     ╱    ╲      ╱    ╲      ╱    ╲     |      |      |      |      |      |
    ╱ x -2 ╲____╱ x  0 ╲____╱ x  2 ╲    | x -2 |______| x  0 |______| x  2 |
    ╲ y  2 ╱    ╲ y  2 ╱    ╲ y  2 ╱    | y  2 |      | y  2 |      | y  2 |
     ╲____╱ x -1 ╲____╱ x  1 ╲____╱     |______| x -1 |______| x  1 |______|
     ╱    ╲ y  1 ╱    ╲ y  1 ╱    ╲     |      | y  1 |      | y  1 |      |
    ╱ x -2 ╲____╱ x  0 ╲____╱ x  2 ╲ == | x -2 |______| x  0 |______| x  2 |
    ╲ y  0 ╱    ╲ y  0 ╱    ╲ y  0 ╱ == | y  0 |      | y  0 |      | y  0 |
     ╲____╱ x -1 ╲____╱ x  1 ╲____╱     |______| x -1 |______| x  1 |______|
     ╱    ╲ y -1 ╱    ╲ y -1 ╱    ╲     |      | y -1 |      | y -1 |      |
    ╱ x -2 ╲____╱ x  0 ╲____╱ x  2 ╲    | x -2 |______| x  0 |______| x  2 |
    ╲ y -2 ╱    ╲ y -2 ╱    ╲ y -2 ╱    | y -2 |      | y -2 |      | y -2 |
     ╲____╱      ╲____╱      ╲____╱     |______|      |______|      |______|
    
    Now the taxicab distance is just the absolute of the coordinates, then
    whatever amount they share in common (subtract the lowest from both,
    keep it) and then, sum in the remainder, dividing by 2 of it's in Y.

---
+++ 2017/11/AOC11a.hs
@@ -0,0 +1,41 @@
+module AOC11a where
+
+import Control.Arrow (first, second)
+import Data.Char
+import Data.List.Split (splitOn)
+
+type Pos = (Int, Int)
+
+x = first
+y = second
+
+inc1 = (+1)
+inc2 = (+2)
+dec1 = subtract 1 -- cries
+dec2 = subtract 2
+
+data Dir = N | NE | SE | S | SW | NW deriving (Eq, Ord, Read, Show)
+
+move :: Dir -> Pos -> Pos
+move N  = y inc2
+move NE = y inc1 . x inc1
+move SE = y dec1 . x inc1
+move S  = y dec2
+move SW = y dec1 . x dec1
+move NW = y inc1 . x dec1
+
+origin :: Pos
+origin = (0, 0)
+
+parseInput :: String -> [Dir]
+parseInput = map (read :: String -> Dir)
+           . splitOn ","
+           . map toUpper
+           . filter (/= '\n')
+
+main :: IO ()
+main = do
+    s <- fmap parseInput $ readFile "input.txt"
+    let s' = foldl (flip move) origin s
+    print s'
+
