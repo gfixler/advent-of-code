4b38202ca816aee6f0693e98f1c074acb2d86aa7
gfixler <gary@garyfixler.com>
2017-12-15 12:52:37 -0800

    WIP on 13b

--- 2017/13/AOC13b.hs
+++ 2017/13/AOC13b.hs
@@ -3,36 +3,22 @@ module AOC13a where
 type Layer = Int
 type Range = Int
 type Pos   = Int
-type Time  = Int
-type Delay = Time
+type Delay = Int
+type Program = (Layer, Range)
 
 parseInput :: String -> (Int, Int)
 parseInput s = case (words s) of
     [l, r] -> (read $ filter (/= ':') l, read r)
     _ -> undefined
 
-scans :: (Layer, Range) -> [Pos]
-scans (_, r) = cycle ([0 .. r-1] ++ [r-2, r-3 .. 1])
+caught :: Program -> Delay -> Bool
+caught (l, r) d = (l - d) `mod` ((r - 1) * 2) == 0
 
-scan :: (Layer, Range) -> Time -> Pos
-scan s = (scans s !!)
-
--- severity :: [(Layer, Range)] -> Delay -> Int
--- severity s t = sum
---              $ map ((\(l, r) -> l * r) . fst)
---              $ filter ((== 0) . snd)
---              $ zip s (map (flip scan t) s)
-
-scanner :: (Layer, Range) -> Time -> String
-scanner (l, r) t = concat [if i == scan (l, r) t then "[*]" else "[ ]" | i <- [0 .. r-1]]
+run :: [Program] -> Delay -> Bool
+run ps d = not $ all id $ map (flip caught d) ps
 
 main :: IO ()
 main = do
     s <- fmap (map parseInput . lines) $ readFile "input.txt"
-    -- let d = map (severity s) [0..]
-    -- print $ length $ takeWhile (/= 0) d
-    -- print $ d !! 22536
-    -- print $ severity s 22536
-    -- print $ map (flip scan 22536) s
-    -- mapM_ print $ takeWhile (/= 0) $ d
-
+    -- print $ length $ takeWhile id $ map (run s) [0..]
+    print $ length $ takeWhile id $ map (run s) [0..]
